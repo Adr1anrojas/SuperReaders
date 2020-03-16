45ebf62b04a11c6b143f7467f07f07bb3a0f6be8
adr1anrojas <adrian.ard@outlook.com>
2020-03-15 21:20:44 -0700

    FrontEnd template init

--- FrontEnd/SuperReaders/src/app/shared/sidebar/sidebar.component.ts
+++ FrontEnd/SuperReaders/src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-
   constructor() { }
-
+  collapsed: Boolean = true;
   ngOnInit(): void {
+  }
+
+  isCollapsed() {
+    this.collapsed = !this.collapsed;
+    $("#wrapper").toggleClass("toggled");
+    console.log(this.collapsed);
   }
 
 }
