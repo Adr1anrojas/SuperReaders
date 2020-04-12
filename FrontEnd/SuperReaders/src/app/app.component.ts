f0a1ee265a2d861c72ffb8e235beee74369cecff
Adrian Alfredo Rojas Duarte <e-aarojas@neoris.com>
2020-04-12 10:52:12 -0600

    add background of login

--- FrontEnd/SuperReaders/src/app/app.component.ts
+++ FrontEnd/SuperReaders/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DateTimeAdapter } from 'ng-pick-datetime';
 import { LoginService } from './services/login.service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,6 @@ export class AppComponent implements OnInit {
   constructor(dateTimeAdapter: DateTimeAdapter<any>, public loginService: LoginService) {
     dateTimeAdapter.setLocale('es');
   }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
