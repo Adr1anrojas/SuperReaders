494d0d0d75559506f0351ec026f68c3c5e3e2640
Adrian Alfredo Rojas Duarte <e-aarojas@neoris.com>
2020-04-12 19:28:28 -0600

    sp

---
+++ Database/SuperReaders.DB/SuperReaders.DB/dbo/StoredProcedures/ClassRoom/ClassRoom_GetAllWithInfo_1.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[ClassRoom_GetAllWithInfo]
+AS
+	BEGIN
+		SELECT 
+			C.[Id],
+			C.[Name], 
+			C.[Status],
+			U.FirstName+' '+U.LastName AS Teacher,
+			COUNT(CD.IdStudent) AS QuantityStudents
+		FROM ClassRoom C
+			LEFT JOIN [Teacher] T ON  C.Id = T.IdClassRoom 
+			LEFT JOIN [User] U ON T.IdUser = U.Id
+			LEFT JOIN ClassRoomDetail CD ON C.Id = CD.IdClassRoom
+		GROUP BY C.[Id], C.[Name], C.[Status], U.FirstName+' '+U.LastName
+		ORDER BY C.Id
+	END
